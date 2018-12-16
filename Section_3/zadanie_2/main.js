b9fd0d7238acdc4682879882e21a0df563694c92
BalisGamer <balis.gamer@gmail.com>
2018-12-16 17:17:27 +0100

    zadanie_2_done

--- Section_3/zadanie_2/main.js
+++ Section_3/zadanie_2/main.js
@@ -1,12 +1,35 @@
 let size = 10;
-let orderElement = 1;
 
 const init = () => {
- // tutaj kod
+
+    const btn = document.createElement("BUTTON");
+    const btn_name = document.createTextNode("Stworz liste")
+    btn.appendChild(btn_name);
+    document.body.appendChild(btn);
+    const ul = document.createElement("ul");
+    ul.setAttribute("id", "list")
+    document.body.appendChild(ul);
+
 }
 
 const createLiElements = () => {
- // tutaj kod
+
+    const ul = document.querySelector("ul");
+    a = (size - 9)
+    y = size
+
+    for (let i = a; i <= y; i++) {
+        const li = document.createElement("li");
+        const li_name = document.createTextNode("Lista nr " + i);
+        li.appendChild(li_name);
+        ul.appendChild(li);
+        li.setAttribute("id", i);
+        document.getElementById(i).style.fontSize = `${size}px`;
+        document.body.appendChild(ul, li);
+        size++;
+    }
 }
 
-init()
+
+
+init(addEventListener("click", createLiElements));
